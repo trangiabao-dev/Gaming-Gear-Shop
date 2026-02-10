8c402e58eea39f8d2afe975a6aeb1909fa9916c7
Trần Gia Bảo <giabao991122@gmail.com>
2026-02-10 23:33:37 +0700

    Bao: Add .gitignore and remove redundant system folders

--- GamingGearShop/src/java/controller/LoginController.java
+++ GamingGearShop/src/java/controller/LoginController.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import static utils.URL.PAGE_HOME;
 import static utils.URL.PAGE_LOGIN;
 
 /**
